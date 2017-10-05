7656c4b203573f4e2a4ac5da30fe1218dd08da09
Fredrik Bogren <fr.bogren@gmail.com>
2017-10-05 15:47:05 +0200

    Updated w/ working DB

---
+++ IU1/Migrations/20171005124518_Initial.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace IU1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cases",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Action = table.Column<string>(nullable: true),
+                    DateOfObservation = table.Column<DateTime>(nullable: false),
+                    Department = table.Column<string>(nullable: true),
+                    Employee = table.Column<string>(nullable: true),
+                    Info = table.Column<string>(nullable: true),
+                    InformerName = table.Column<string>(nullable: false),
+                    InformerPhone = table.Column<string>(nullable: false),
+                    Observation = table.Column<string>(nullable: true),
+                    Place = table.Column<string>(nullable: false),
+                    RefNumber = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    TypeOfCrime = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cases", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentID = table.Column<string>(nullable: false),
+                    DepartmentName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<string>(nullable: false),
+                    Department = table.Column<string>(nullable: true),
+                    EmployeeName = table.Column<string>(nullable: true),
+                    RoleTitle = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    StatusID = table.Column<string>(nullable: false),
+                    StatusName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.StatusID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cases");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+        }
+    }
+}
---
+++ IU1/Migrations/ApplicationDBContextModelSnapshot.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using IU1.Models;
+
+namespace IU1.Migrations
+{
+    [DbContext(typeof(ApplicationDBContext))]
+    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("IU1.Models.Case", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Action");
+
+                    b.Property<DateTime>("DateOfObservation");
+
+                    b.Property<string>("Department");
+
+                    b.Property<string>("Employee");
+
+                    b.Property<string>("Info");
+
+                    b.Property<string>("InformerName")
+                        .IsRequired();
+
+                    b.Property<string>("InformerPhone")
+                        .IsRequired();
+
+                    b.Property<string>("Observation");
+
+                    b.Property<string>("Place")
+                        .IsRequired();
+
+                    b.Property<string>("RefNumber");
+
+                    b.Property<string>("Status");
+
+                    b.Property<string>("TypeOfCrime")
+                        .IsRequired();
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Cases");
+                });
+
+            modelBuilder.Entity("IU1.Models.Department", b =>
+                {
+                    b.Property<string>("DepartmentID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("DepartmentName");
+
+                    b.HasKey("DepartmentID");
+
+                    b.ToTable("Departments");
+                });
+
+            modelBuilder.Entity("IU1.Models.Employee", b =>
+                {
+                    b.Property<string>("EmployeeID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Department");
+
+                    b.Property<string>("EmployeeName");
+
+                    b.Property<string>("RoleTitle");
+
+                    b.HasKey("EmployeeID");
+
+                    b.ToTable("Employees");
+                });
+
+            modelBuilder.Entity("IU1.Models.Status", b =>
+                {
+                    b.Property<string>("StatusID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("StatusName");
+
+                    b.HasKey("StatusID");
+
+                    b.ToTable("Statuses");
+                });
+        }
+    }
+}
